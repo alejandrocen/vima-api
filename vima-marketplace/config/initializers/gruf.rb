d31ba988a048746cb3ea472312b2037a5eb656ae
Alejandro Cen <alex96.cen@gmail.com>
2022-09-06 08:20:18 -0500

    Remove custom logger

--- vima-marketplace/config/initializers/gruf.rb
+++ vima-marketplace/config/initializers/gruf.rb
@@ -2,7 +2,6 @@
 
 Gruf.configure do |config|
   config.default_client_host = ENV.fetch('DEFAULT_CLIENT_HOST', 'localhost:50051')
-  config.logger = Logger.new($stdout)
   config.interceptors.use(
     Gruf::Interceptors::Instrumentation::RequestLogging::Interceptor,
     formatter: :logstash
